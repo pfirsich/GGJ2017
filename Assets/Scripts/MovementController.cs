0fbb69dbf8d9ce54e5fe5476535c1e730810072a
joelschum@gmail.com <joelschum@gmail.com>
2017-01-21 05:14:47 +0100

    Implemented first version of movement

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -3,30 +3,34 @@ using System.Collections;
 
 public class MovementController : MonoBehaviour
 {
-    [SerializeField]
-    GameObject player;
+    private float minSpeed = 1.0f;
+    private float maxSpeed = 5.0f;
+    [SerializeField] float acceleration;
 
-    [SerializeField]
-    float speed;
+    private Rigidbody rigidBody;
 
-    Vector3 direction;
-	// Use this for initialization
 	void Start ()
 	{
-        direction = transform.position - player.transform.position;
+        rigidBody = GetComponent<Rigidbody>();
+        rigidBody.velocity = new Vector3(0.0f, 0.0f, maxSpeed);
     }
-	
+
 	// Update is called once per frame
 	void Update ()
 	{
-        player.transform.Translate(player.transform.forward*speed*Time.deltaTime);
-	    float vertical = Input.GetAxis("Vertical");
-	    float horizontal = Input.GetAxis("Horizontal");
-	    Vector3 movementAxis = Vector3.up * vertical + Vector3.right * horizontal;
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+        rigidBody.AddRelativeForce(move * acceleration);
 
+        float speed = rigidBody.velocity.magnitude;
+        if(speed < minSpeed) rigidBody.velocity *= minSpeed / speed;
+        if(speed > maxSpeed) rigidBody.velocity *= maxSpeed / speed;
 
-	    Quaternion rot = Quaternion.FromToRotation();
-        transform.position = player.transform.position + rot * direction;
-        transform.localRotation = rot;
+        transform.LookAt(rigidBody.position + rigidBody.velocity);
+
+        //player.transform.Translate(player.transform.forward*speed*Time.deltaTime);
+	    //Vector3 movementAxis = Vector3.up * vertical + Vector3.right * horizontal;
+	    //Quaternion rot = Quaternion.FromToRotation();
+        //transform.position = player.transform.position + rot * direction;
+        //transform.localRotation = rot;
     }
 }
