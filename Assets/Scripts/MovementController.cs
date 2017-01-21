3186a68acc58ec0282b013b067b2f8e480729344
Simon Schliesky <schliesky.simon+git@gmail.com>
2017-01-21 01:12:08 +0100

    basic (broken) movement

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementController : MonoBehaviour
+{
+    [SerializeField]
+    GameObject player;
+
+    [SerializeField]
+    float speed;
+	// Use this for initialization
+	void Start ()
+	{
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+	{
+        player.transform.Translate(player.transform.forward*speed*Time.deltaTime);
+	    float vertical = Input.GetAxis("Vertical");
+	    float horizontal = Input.GetAxis("Horizontal");
+
+	    Vector3 movementAxis = transform.up * vertical + transform.right * horizontal;
+        player.transform.localRotation = Quaternion.FromToRotation(player.transform.forward, movementAxis.normalized);
+    }
+}
