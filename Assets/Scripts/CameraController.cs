31a97d917ff806e74f1b67e1e88d5f7c0030bf54
MoritzKn <moritz.kneilmann@gmx.de>
2017-01-21 17:56:49 +0100

    Move camera down

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,14 +7,14 @@ public class CameraController : MonoBehaviour {
     [SerializeField] float targetDistance = 10.0f;
     [SerializeField] float lookAhead = 50.0f;
     [SerializeField] float lookAtSpeed = 10.0f;
-    [SerializeField] Vector3 lookAtPosition;
     private Vector3[] lastPlayerPos;
     private const int lastPlayerPosCount = 10;
     private int nextPlayerPosIndex = 0;
 
+	float yOffset = 4;
+
 	// Use this for initialization
 	void Start () {
-        lookAtPosition = player.transform.position;
         lastPlayerPos = new Vector3[lastPlayerPosCount];
 	}
 
@@ -32,7 +32,7 @@ public class CameraController : MonoBehaviour {
         }
         forward /= lastPlayerPosCount;
 
-        Vector3 rel = player.transform.position - transform.position;
+		Vector3 rel = (player.transform.position + new Vector3(0, yOffset, 0)) - transform.position;
         transform.position += rel.normalized * (rel.magnitude - targetDistance);// * approachSpeed * Time.deltaTime;
         transform.LookAt(player.transform.position + forward * lookAhead);
     }
